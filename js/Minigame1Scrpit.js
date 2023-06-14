f811f9547516827b6f27594971282dcda52eec5c
FelixKohlboeck <f.kohlboeck@students.htl-leonding.ac.at>
2023-06-14 22:25:34 +0200

    Changed the switch/case statements to one if with a for loop. function AskEmotion is now shorter. Emotions are also saved in an array

--- js/Minigame1Scrpit.js
+++ js/Minigame1Scrpit.js
@@ -1,4 +1,5 @@
 const video = document.getElementById('video')
+const emotionArray = ['happy', 'sad', 'neutral', 'surprised']
 
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
@@ -24,9 +25,6 @@ video.addEventListener('play', () => {
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
     const resizedDetections = faceapi.resizeResults(detections, displaySize);
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-    faceapi.draw.drawDetections(canvas, resizedDetections);
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
     
 
     // Überprüfe, ob Gesichter erkannt wurden
@@ -34,8 +32,11 @@ video.addEventListener('play', () => {
       // Extrahiere den ersten erkannten Gesichtsausdruck
       const expressions = resizedDetections[0].expressions;
 
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
       // Erstelle einen String, der den Gesichtsausdruck repräsentiert
-      let emotion = ""; 
+      let emotion = "";
       for (const [expression, probability] of Object.entries(expressions)) {
         if (probability > 0.1) {
           emotion = expression;
@@ -50,83 +51,38 @@ video.addEventListener('play', () => {
   }, 100);
 });
 
-
 let next = true;
 let num = 0;
 let wellDoneTimer = null;
 
-async function AskEmotion(emotion) {
-
+function AskEmotion(emotion) {
   const textContainer = document.getElementById("TextForEmotion");
 
-  if(next == true) {
+  if (next == true) {
     num = getRandomNumber(1, 4);
     next = false;
   }
-
-  switch(num) {
-
-    case 1:
-
-      textContainer.innerText = "Show me your happy Face";
-      if (emotion == 'happy') {
-        GotRightEmotion(textContainer);
+  else if(next == false){
+    for(let i = 1; i <= emotionArray.length; i++)
+    {
+      textContainer.innerText = 'Show me your ' + emotionArray[num - 1] + ' face';
+      if(emotion == emotionArray[num - 1]){
+        next = true;
+        textContainer.innerText = 'Well Done';
+        startWellDoneTimer(textContainer);
       }
-    break;
-    case 2:
-
-      textContainer.innerText = "Show me your sad Face";
-      if (emotion == 'sad') {
-        GotRightEmotion(textContainer);
-      }
-    break;
-    case 3:
-
-      textContainer.innerText = "Show me your neutral Face";
-      if (emotion == 'neutral') {
-        GotRightEmotion(textContainer);
-      }
-    break;
-    case 4:
-
-      textContainer.innerText = "Show me your surprised Face";
-      if (emotion == 'surprised') {
-        GotRightEmotion(textContainer);
-      }
-    break;
-    case 5:
-
-      textContainer.innerText = "Show me your angry Face";
-      if (emotion == 'angry') {
-        GotRightEmotion(textContainer);
-      }
-    break;
-    case 6:
-
-      textContainer.innerText = "Show me your disgusted Face";
-      if (emotion == 'disgusted') {
-        GotRightEmotion(textContainer);
-      }
-    break;
-    default:
-      console.log("geht nicht");
+    }
   }
 }
 
-function GotRightEmotion(textContainer) {
-  
-  next = true;
-  textContainer.innerText = "Well done";
-  startWellDoneTimer(textContainer);
-}
-
-async function startWellDoneTimer(textContainer) {
-  clearTimeout(wellDoneTimer);
+function startWellDoneTimer(textContainer) {
+  clearTimeout(wellDoneTimer); // Zurücksetzen des vorherigen Timers, falls vorhanden
   wellDoneTimer = setTimeout(function () {
-    textContainer.innerText = "";
-  }, 5000);
+    textContainer.innerText = ""; // Leere den TextContainer nach 5 Sekunden
+  }, 5000); // Anzeigezeit von 5 Sekunden
 }
 
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
